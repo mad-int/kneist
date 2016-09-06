d22fe9833a97341426f620bb31bd715d33d48095
int <int@arcor.de>
2016-09-06 11:44:10 +0200

    Added logging of HTTP-Request and-Response.
    Added also examples of setting up the HTTP-Header.

--- checklinks.pl
+++ checklinks.pl
@@ -10,16 +10,27 @@ use Data::Dumper;
 
 #
 
+# Which encodings perl can handle.
+# deflated e.g. no encoding should always be available.
+# Other possiblities: gzip.
+#print join(",", HTTP::Message::decodable())."\n";
+
 my $bot = WWW::Mechanize->new();
 #print Dumper($bot);
+#$bot->default_header('Accept-Language' => "de, en");
+
+$bot->add_handler("request_send", sub { $_[0]->dump(); return; });
+$bot->add_handler("response_done", sub { shift->dump; return; });
 
 my $starturl = "http://www.stephanlenhart.de/";
 
-my $resp = $bot->get($starturl);
+my $resp = $bot->get($starturl, 'Accept-Encoding' => 'deflate');
 if(not $resp->is_success())
 {
     die("Couldn't fetch url: `".$starturl."'");
 }
+
+print "\n";
 
 my @linklist = $bot->links();
 @linklist = map { $_->url_abs(); } @linklist;
